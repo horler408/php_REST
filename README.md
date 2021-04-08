d920b5086e177a5627ae8cb2d05627b6df5a4f32
horler408 <horler.web@gmail.com>
2021-04-08 12:37:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple php REST API
---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database{
+  
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "api_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $e){
+            echo "Connection error: " . $e->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
+?>
